3efaf77704b12f05c9bab5547bfc6c8c37485236
TomBen <mr.ren.tao123@gmail.com>
2021-03-28 20:30:46 +0800

    Update

--- boltzmann_wealth_model/money_model.py
+++ boltzmann_wealth_model/money_model.py
@@ -1,3 +1,34 @@
-# From https://mesa.readthedocs.io/en/master/tutorials/intro_tutorial.html
+# Code: https://mesa.readthedocs.io/en/master/tutorials/intro_tutorial.html
+# Model: http://arxiv.org/abs/cond-mat/0211175
 
 
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+
+class MoneyAgent(Agent):
+    '''An agent with fixed initial wealth.'''
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.wealth = 1
+
+    def step(self):
+        # The agent’s step will go here.
+        # For demostration purposes we will print the agent‘s unique_id
+        print(f'Hi, I am agent {self.unique_id}, and I have {self.wealth} unit of money.')
+
+class MoneyModel(Model):
+    '''A model with some number of agents.'''
+    def __init__(self, N):
+        self.num_agents = N
+        self.schedule = RandomActivation(self)
+        # Create agents
+        for i in range(self.num_agents):
+            a = MoneyAgent(i, self)
+            self.schedule.add(a)
+
+    def step(self):
+        '''Advance the model by one step.'''
+        self.schedule.step()
+
+empety_model = MoneyModel(10)
+empety_model.step()
