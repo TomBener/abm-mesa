67f986484d05c4302a581f2323266b7445eae6e3
TomBen <mr.ren.tao123@gmail.com>
2021-05-10 17:42:00 +0800

    Update forest fire model
    赶紧突然开了窍 🤣️

--- forest_fire/agent.py
+++ forest_fire/agent.py
@@ -12,7 +12,8 @@ class TreeCell(Agent):
             - Burned Out
         unique_id: (x, y) tuple.
 
-    unique_id isn’t strictly necessary here, but it’s good practice to give one to each agent anyway.
+    unique_id isn’t strictly necessary here,
+    but it’s good practice to give one to each agent anyway.
     '''
 
     def __init__(self, pos, model):
@@ -26,12 +27,36 @@ class TreeCell(Agent):
         self.pos = pos
         # Initial condition is 'Fine'. That’s fine.
         self.condition = 'Fine'
+        # self.status = 'good'  self.placeholder can be defined here
 
         def step(self):
             '''
             If the tree is on fire, spread it to fine tree nearby.
             '''
             if self.condition == 'On Fire':
+                '''
+                Please note that mesa/agent.py has imported the Model class, that is:
+                from mesa.model import Model
+
+                Python uses a dot (.) instead of a slash (/) to refer to the file path when importing packages or modules.
+                
+                PEP 8 explicitly recommends absolute imports.
+
+                Be same as the Unix-like operating system. Relative imports make use of dot notation to specify location:
+
+                # A single dot means that the module or package referenced is in the same directory as the current location.
+                from .some_module import some_class
+
+                # Two dots mean that it is in the parent directory of the current location
+                from ..some_package import some_function
+
+                # A bare dot means the current directory
+                from . import some_clas
+
+                self.model.grid.neighbor_iter is a method,
+                which is defined in mesa/space.py.
+                neighbor_iter: Iterates over position neighbors.
+                '''
                 for neighbor in self.model.grid.neighbor_iter(self.pos):
                     if neighbor.condition == 'Fine':
                         neighbor.condition = 'On Fire'
--- forest_fire/model.py
+++ forest_fire/model.py
@@ -15,15 +15,24 @@ class ForestFire(Model):
         Create a new forest fire model.
 
         Args:
-            - height, width: the size of the grid to model
-            - density: what fraction of grid cells have a tree in them.
+            - height: the grid's height
+            - width: the grid's width
+            - density: what fraction of grid cells have a tree in them (initially)
         '''
-        # Set up model ojects
+
+        '''
+        Set up model ojects
+        
+        RandomActivation is a schedular which actives each agent once per step,
+        in random order, with the order reshuffled every step.
+        This is equivalent to the NetLogo 'ask agent…' and is generally the
+        default behavior for an ABM
         self.schedule = RandomActivation(self)
         self.grid = Grid(height, width, torus=False)
-
+        '''
         self.datacollector = DataCollector(
             {
+                # lambda function, it is like: lambda x, y: x ** y
                 'Fine': lambda m: self.count_type(m, 'Fine'),
                 'On Fire': lambda m: self.count_type(m, 'On Fire'),
                 'Burned Out': lambda m: self.count_type(m, 'Burned Out'),
@@ -31,6 +40,7 @@ class ForestFire(Model):
         )
 
         # Place a tree in each cell with Prob = density (here is 0.65)
+        # coord_iter: returns coordinates as well as cell contents.
         for (contents, x, y) in self.grid.coord_iter():
             if self.random.random() < density:
                 # Create a tree
@@ -38,7 +48,9 @@ class ForestFire(Model):
                 # Set all trees in the first column on fire.
                 if x == 0:
                     new_tree.condition = 'On Fire'
+                # place_agent: positions an agent on the grid, and set its pos variable.
                 self.grid._place_agent((x, y), new_tree)
+                # Add an agent object to the schedule.
                 self.schedule.add(new_tree)
 
         self.running = True
@@ -56,12 +68,20 @@ class ForestFire(Model):
         if self.count_type(self, 'On Fire') == 0:
             self.running = False
 
-    @staticmethod
+    @staticmethod  # I don’t know this method
     def count_type(model, tree_condition):
         '''
         Helper method to count trees in a given condition in a given model.
         '''
+        # Bear in mind this kind method
+        # initial score is 0, which is very helpful.
         count = 0
+        '''
+        The default DataCollector has sevaral assumptions:
+            - The model has a schedule object called 'schedule'
+            - The schedule has an agent list called 'agents'
+            - For collecting agent-level variables, agents must have a unique_id
+        '''
         for tree in model.schedule.agents:
             if tree.condition == tree_condition:
                 count += 1
